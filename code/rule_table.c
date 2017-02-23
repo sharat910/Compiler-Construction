58136717745681c37ebeb3dc28d57ead644c33cf
rishabh065 <rishabh.4397@gmail.com>
2017-02-23 16:28:32 +0530

    tokenizer and maps implemented

--- code/rule_table.c
+++ code/rule_table.c
@@ -2,3 +2,15 @@
 #include <stdio.h>
 
 rule rules[100];
+
+void rule_table_init()
+{
+	char str[50];
+	FILE* fp=fopen("grammer.txt","r");
+	while(fscanf(fp,"%s",str) != EOF)
+	{
+		int l=atoi(s);
+		fscanf(fp,"%s",str);
+		sprintf()
+	}
+}
