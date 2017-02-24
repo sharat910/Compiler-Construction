823cc1d5dd5ad82cb16c0d80e76cd85dd509e6d8
sharat910 <sharat910@gmail.com>
2017-02-24 12:16:13 +0530

    Cleaning and added reverse grammar

--- First Follow/line_num.py
+++ First Follow/line_num.py
@@ -1,4 +1,4 @@
-with open("ggrammar.txt","r") as f:
+with open("line_separated_grammar.txt","r") as f:
 	lines = f.read().splitlines()
 
 def write_line(f,line):
@@ -15,12 +15,23 @@ def usefull(string):
 def clean(x):
 	return x.strip()
 
-with open("line_sep_grammar.txt",'w') as f:	
+with open("line_numbered_grammar_normal.txt",'w') as f:	
 	for i,line in enumerate(lines):
 		lhs,rhs = map(clean,line.split("->"))
 		write_line(f,str(i + 1))
 		write_line(f,str(lhs))
 		rhs_terms = filter(usefull,rhs.split(" "))
 		write_line(f,str(len(rhs_terms)))
+		for term in rhs_terms:
+			write_line(f,str(term))
+
+with open("line_numbered_grammar_reverse.txt",'w') as f:	
+	for i,line in enumerate(lines):
+		lhs,rhs = map(clean,line.split("->"))
+		write_line(f,str(i + 1))
+		write_line(f,str(lhs))
+		rhs_terms = filter(usefull,rhs.split(" "))
+		write_line(f,str(len(rhs_terms)))
+		rhs_terms.reverse()
 		for term in rhs_terms:
 			write_line(f,str(term))
